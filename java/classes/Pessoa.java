7f04e0d5f8c6cdda58d91ce0241f878ec3ffa5d7
vlavarda <vlavarda@aawz.com.br>
2019-04-26 19:01:29 -0300

    Super Classe pessoa

--- java/classes/Pessoa.java
+++ java/classes/Pessoa.java
@@ -1,3 +1,99 @@
-public class Usuario {
+package classes;
 
+public class Pessoa {
+	
+	private String nome;
+	private String email;
+	private String numeroCelular;
+	private String cpf;
+	private int rg;
+	private String dataNascimento;
+	private String endereco; // Digitar CPF fazer busca automatizca do endereço
+	private char sexo;
+	private String estadoCivil;
+	
+	
+	public Pessoa(String nome,String email,String numeroCelular, String cpf, int rg,String dataNascimento, String endereco, char sexo, String estadoCivil) {
+		this.nome = nome;
+		this.email = email;
+		this.numeroCelular = numeroCelular;
+		this.cpf = cpf;
+		this.rg = rg;
+		this.dataNascimento = dataNascimento;
+		this.endereco = endereco;
+		this.sexo = sexo;
+		this.estadoCivil = estadoCivil;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNumeroCelular() {
+		return numeroCelular;
+	}
+
+	public void setNumeroCelular(String numeroCelular) {
+		this.numeroCelular = numeroCelular;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public int getRg() {
+		return rg;
+	}
+
+	public void setRg(int rg) {
+		this.rg = rg;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
 }
