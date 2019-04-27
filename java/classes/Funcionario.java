e986b2d40d144fce918c08349f071fea1d549cd1
Guilherme Steinert <steinert999@outlook.com>
2019-04-27 16:37:54 -0300

    adicionado classes Funcionario, Gerente e InterfaceCadastros

---
+++ java/classes/Funcionario.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Funcionario extends Pessoa {
+	
+	public Funcionario(String nome,String email,String numeroCelular,
+			String cpf,	int rg,	String dataNascimento,
+			String endereco, char sexo,	String estadoCivil){
+		super(nome,	email, numeroCelular, cpf, rg,
+				dataNascimento,	endereco, sexo, estadoCivil);
+			}
+
+}
