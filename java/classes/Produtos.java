cc31e20f8bdbc82fb370a853771adaf06c70b782
isaiasac <47405566+isaiasac@users.noreply.github.com>
2019-04-27 01:57:38 -0300

    Produtos.java

--- java/classes/Produtos.java
+++ java/classes/Produtos.java
@@ -1,4 +1,34 @@
 package classes;
-public class Produtos {
 
+public class Produtos { 
+  
+    private String codigoProduto;
+	  private String descricao;
+    private float preco;
+  
+    public Produto(String codigoProduto, String descricao, float preco){
+    
+      this.codigoProduto = codigoProduto;
+      this.descricao = descricao;
+      this.preco = preco;
+    }
+    public String getCodigoProduto() {
+		    return codigoProduto;
+	  }
+	  public void setCodigoProduto(String codigoProduto) {
+		    this.codigoProduto = codigoProduto;
+  	}
+  	public String getDescricao() {
+	    	return descricao;
+  	}
+  	public void setDescricao(String descricao) {
+	    	this.descricao = descricao;
+	  }
+    public float getPreco() {
+	    	return preco;
+  	}
+	  public void setPreco(float preco) {
+	    	this.preco = preco;
+	  }
+  
 }
