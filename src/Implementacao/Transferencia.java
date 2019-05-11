30e7ef0673b6467cbd398e4813809adca9771ffa
vlavarda <vlavarda@aawz.com.br>
2019-05-11 18:48:15 -0300

    Fazendo integração com o db

--- src/Implementacao/Transferencia.java
+++ src/Implementacao/Transferencia.java
@@ -1,52 +1,109 @@
 package Implementacao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Transferencia {
-	private int cod_transferencia;
-	private int cod_produto ;
-	private int cod_fornecedor; // mb not,transferencia apenas entre filiais
-	private int cod_filial;
 	
-	public Transferencia(int cod_transferencia, int cod_produto, int cod_fornecedor, int cod_filial) {
-		this.cod_transferencia = cod_transferencia;
-		this.cod_produto = cod_produto;
-		this.cod_fornecedor = cod_fornecedor;
-		this.cod_filial = cod_filial;
-	}
-
-	public int getCod_transferencia() {
-		return cod_transferencia;
-	}
-
-	public void setCod_transferencia(int cod_transferencia) {
-		this.cod_transferencia = cod_transferencia;
-	}
-
-	public int getCod_produto() {
-		return cod_produto;
-	}
-
-	public void setCod_produto(int cod_produto) {
-		this.cod_produto = cod_produto;
-	}
-
-	public int getCod_fornecedor() {
-		return cod_fornecedor;
-	}
-
-	public void setCod_fornecedor(int cod_fornecedor) {
-		this.cod_fornecedor = cod_fornecedor;
-	}
-
-	public int getCod_filial() {
-		return cod_filial;
-	}
-
-	public void setCod_filial(int cod_filial) {
-		this.cod_filial = cod_filial;
+	private ConnectionDB db = new ConnectionDB();
+	private int codTransferencia;
+	private int codProduto ;
+	private int codFornecedor; // mb not,transferencia apenas entre filiais
+	private int codFilial;
+	private String dataSaida;
+	
+	public Transferencia() {
+		
 	}
 	
-	public void transferenciaProduto() {
-		// Pegar cod_produto, cod_filial que foi transferido do banco
+	public Transferencia(int codTransferencia, int codProduto, int codFornecedor, int codFilial, String dataSaida) {
+		this.codTransferencia = codTransferencia ;
+		this.codProduto = codProduto; 
+		this.codFornecedor = codFornecedor;
+		this.codFilial = codFilial;
+		this.dataSaida = dataSaida;
+	}
+	
+	public int getCodTransferencia() {
+		return codTransferencia;
+	}
+
+	public void setCodTransferencia(int codTransferencia) {
+		this.codTransferencia = codTransferencia;
+	}
+
+	public int getCodProduto() {
+		return codProduto;
+	}
+
+	public void setCodProduto(int codProduto) {
+		this.codProduto = codProduto;
+	}
+
+	public int getCodFornecedor() {
+		return codFornecedor;
+	}
+
+	public void setCodFornecedor(int codFornecedor) {
+		this.codFornecedor = codFornecedor;
+	}
+
+	public int getCodFilial() {
+		return codFilial;
+	}
+
+	public void setCodFilial(int codFilial) {
+		this.codFilial = codFilial;
+	}
+
+	public String getDataSaida() {
+		return dataSaida;
+	}
+
+	public void setDataSaida(String dataSaida) {
+		this.dataSaida = dataSaida;
+	}
+	
+	public void mostrarTransferencia() {
+		System.out.println("cod_transferencia cod_produto cod_fornecedor cod_filial data_saida");
+		System.out.println(this.getCodTransferencia() + " " + this.getCodProduto() + " " +
+						   this.getCodFornecedor() + " " + this.getCodFilial() + " " + this.getDataSaida() );
+	}
+	
+	public void transferirProduto(int codTransferencia, int codProduto, int codFornecedor, int codFilial, String dataSaida) throws SQLException {
+		db.Conectar();
+		String qr = "insert into saida_produtos(cod_produto,cod_fornecedor,cod_filial,dt_saida_produto) values(?,?,?,?)";
+		PreparedStatement stm = db.preparedStament(qr);
+		
+		stm.setInt(1, codTransferencia);
+		stm.setInt(2, codProduto);
+		stm.setInt(3, codFilial);
+		stm.setString(4, dataSaida);
+		
+		db.runPreparedStatment(stm);
+		
+		System.out.println("Transferencia realizada com sucesso!");
+		db.Desconectar();
+	}
+
+	public void historicoTransferencias() throws SQLException {
+		db.Conectar();
+		ResultSet result = db.SelectQuery("select * from saida_produtos");
+		System.out.println("Historico de transferencias:");
+		while (result.next() ) {
+			Transferencia transf = new Transferencia(result.getInt("cod_saida_produto"),result.getInt("cod_produto"), result.getInt("cod_fornecedor"), result.getInt("cod_filial"),result.getString("dt_saida_produto"));
+			transf.mostrarTransferencia();
+		}
+		
+		db.Desconectar();
+		
+	}
+	
+	public static void main(String args[]) throws SQLException {
+		Transferencia c = new Transferencia();
+		c.historicoTransferencias();
+		c.transferirProduto(6, 2, 10 , 2, "27/05/21");
 	}
 	
 }
