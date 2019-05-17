8c622e3e7a36d3da9f13596f55bd164b14aad42a
isaiasac <47405566+isaiasac@users.noreply.github.com>
2019-05-17 19:07:39 -0300

    Update

--- src/Implementacao/ConnectionDB.java
+++ src/Implementacao/ConnectionDB.java
@@ -11,10 +11,10 @@ public class ConnectionDB {
     private PreparedStatement stm = null; // Query modificavel
     private ResultSet result; // Resultado da query
     // HOST PADRÂO USAR QUANDO ESTIVER SEM O DB "jdbc:postgresql://127.0.0.1:5432/"
-    private String URL = "jdbc:postgresql://192.168.4.204:5432/"; // url do servidor
-    private String USER = "groupaps"; // usuario do db
-    private String PASSWORD = "aps2019-1"; // senha do usuario
-    private String DATABASE = "projeto_estoque"; // banco 
+    private String URL = "jdbc:postgresql://127.0.0.1:5432/"; // url do servidor 192.168.4.204:5432
+    private String USER = "postgres"; // usuario do db groupaps
+    private String PASSWORD = "postgres"; // senha do usuario aps2019-1
+    private String DATABASE = "produtos_aps"; // banco projeto_estoque
     
     public void Conectar() {
         
--- src/Implementacao/Produtos.java
+++ src/Implementacao/Produtos.java
@@ -1,12 +1,13 @@
 package Implementacao;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.text.DateFormat;
-import java.util.*;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.text.DateFormat;
+//import java.util.*;
 import java.sql.*;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 public class Produtos { 
     private int codigoProduto;
@@ -57,50 +58,64 @@ public class Produtos {
     }
     public void inserir() throws Exception {
     	db.Conectar();
-        String sql = "INSERT INTO PRODUTO (nome_produto, preco_produto, cod_categoria) VALUES (?, ?, ?)";
+       try {
+    	 String sql = "INSERT INTO PRODUTOS (nome_produto, preco_produto, cod_categoria) VALUES (?, ?, ?)";
         PreparedStatement stm = db.preparedStament(sql);
         stm.setString(1, this.getNomeProduto());
         stm.setDouble(2, this.getPreco());
         stm.setInt(3,this.getCodigoCategoria());
         db.runPreparedStatment(stm);
-		db.Desconectar();
+		JOptionPane.showMessageDialog(null, "Produto cadastrado com exito.");
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Erro ao inserir dados /nErro: "+ex);
+		}
+       db.Desconectar();
     }
     public void alterar() throws Exception {
     	db.Conectar();
-        String sql = "UPDATE PRODUTO SET nome_produto=?, preco_produto=?,cod_categoria=? WHERE cod_produto=?";
-        PreparedStatement stm = db.preparedStament(sql);
-        stm.setString(1, this.getNomeProduto());
-        stm.setDouble(2, this.getPreco());
-        stm.setInt(3, this.getCodigoCategoria());
-        stm.setInt(4, this.getCodigoProduto());
-        db.runPreparedStatment(stm);
-		db.Desconectar();
+    	try{
+    		String sql = "UPDATE PRODUTOS SET nome_produto=?, preco_produto=?,cod_categoria=? WHERE cod_produto=?";
+    		PreparedStatement stm = db.preparedStament(sql);
+    		stm.setString(1, this.getNomeProduto());
+    		stm.setDouble(2, this.getPreco());
+    		stm.setInt(3, this.getCodigoCategoria());
+    		stm.setInt(4, this.getCodigoProduto());
+    		db.runPreparedStatment(stm);
+    		JOptionPane.showMessageDialog(null, "Produto alterado com exito.");
+    	} catch (SQLException ex) {
+    		JOptionPane.showMessageDialog(null,"Erro ao alterar dados /nErro: "+ex);
+    	}
+    	db.Desconectar();
     }
     public void excluir() throws Exception {
     	db.Conectar();
-        String sql = "DELETE FROM PRODUTO WHERE cod_produto=?";
-        PreparedStatement stm = db.preparedStament(sql);
-        stm.setInt(1, this.getCodigoProduto());
-        db.runPreparedStatment(stm);
-		db.Desconectar();
+        try{
+        	String sql = "DELETE FROM PRODUTOS WHERE cod_produto=?";
+        	PreparedStatement stm = db.preparedStament(sql);
+        	stm.setInt(1, this.getCodigoProduto());
+        	db.runPreparedStatment(stm);
+        	JOptionPane.showMessageDialog(null, "Produto excluido com exito.");
+        } catch (SQLException ex) {
+        	JOptionPane.showMessageDialog(null, "Erro ao excluir dados /nErro: "+ex);
+        }
+        db.Desconectar();
     }
     public ArrayList<Produtos> listarTodos() throws Exception {
     	db.Conectar();
-        String sql = "SELECT * FROM PRODUTO ORDER BY nome_produto";
-        PreparedStatement stm = db.preparedStament(sql);
-        ResultSet lp = stm.executeQuery();
+    		String sql = "SELECT * FROM PRODUTOS ORDER BY nome_produto";
+    		PreparedStatement stm = db.preparedStament(sql);
+    		ResultSet lp = stm.executeQuery();
 
-        ArrayList<Produtos> listaProdutos = new ArrayList<Produtos>();
-        while (lp.next()) {
-            Produtos produtos = new Produtos(codigoProduto, nomeProduto, preco, codigoCategoria);
-            produtos.setCodigoProduto(lp.getInt("codigo_produto"));
-            produtos.setNomeProduto(lp.getString("nome_produto"));
-            produtos.setPreco(lp.getDouble("preco_produto"));
-            produtos.setCodigoCategoria(lp.getInt("cod_categoria"));
+    		ArrayList<Produtos> listaProdutos = new ArrayList<Produtos>();
+    		while (lp.next()) {
+    			Produtos produtos = new Produtos(codigoProduto, nomeProduto, preco, codigoCategoria);
+    			produtos.setCodigoProduto(lp.getInt("codigo_produto"));
+    			produtos.setNomeProduto(lp.getString("nome_produto"));
+    			produtos.setPreco(lp.getDouble("preco_produto"));
+    			produtos.setCodigoCategoria(lp.getInt("cod_categoria"));
  
             listaProdutos.add(produtos);
         }
-
         return listaProdutos;
     }
     public static void main(String[]args) throws Exception {
