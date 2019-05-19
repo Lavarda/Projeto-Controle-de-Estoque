996f9c726485147e8700b59c4edeeabbdf288a08
Guilherme Steinert <steinert999@outlook.com>
2019-05-18 21:22:40 -0300

    Realizado melhorias nos metodos da Classe Funcionário

--- src/Implementacao/Autenticacao.java
+++ src/Implementacao/Autenticacao.java
@@ -1,5 +1,5 @@
 package Implementacao;
 
 public interface Autenticacao {
-	void realizarAutenticacao(String email, String senha);
+	void realizarAutenticacao(String matricula, String senha);
 }
--- src/Implementacao/Funcionario.java
+++ src/Implementacao/Funcionario.java
@@ -7,93 +7,61 @@ import java.sql.SQLException;
 import Connections.ConnectionDB;
 
 public class Funcionario extends Pessoa implements Autenticacao{
-	//private Cargos cargo;
-	//private String dataAdmissao;
+	private Cargos cargo;
 	private String senha;
+	private String matricula;
 	private static ConnectionDB conexao = new ConnectionDB();
-	public Funcionario(String email,String senha,String cpf) {
+	public Funcionario(String matricula,String nome,String senha) {
 		this.senha = senha;
-		this.setEmail(email);
-		this.setCpf(cpf);
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	public Funcionario(String nome,String email,String cpf,String rg,String dataNascimento) {
+		this.matricula = matricula;
 		this.setNome(nome);
-		this.setEmail(email);
-		this.setCpf(cpf);
-		this.setRg(rg);
-		this.setDataNascimento(dataNascimento);	
 	}
-	public void consultarfuncionario(String cpf){
+	public void consultarfuncionario(){
 			try {
+				String nomeFuncionario = null;
+				String cargoFuncionario = null;
+				String matriculaFuncionario = null;
+				String salarioFuncionario = null;
+				String telefoneFuncionario = null;
+				String sql = "SELECT * FROM vw_funcionarios WHERE matricula_funcionario = ?";
 				conexao.Conectar();
-				String sql = "SELECT * FROM funcionarios WHERE cpf_funcionario = ?";
 				PreparedStatement preparedStatement = conexao.preparedStament(sql);
-				preparedStatement.setString(1, cpf);
+				preparedStatement.setString(1, this.getMatricula());
 				ResultSet result = conexao.runPreparedSelect(preparedStatement);
 				while(result.next()) {
-					String codFuncionario,emailFuncionario,nomeFuncionario,cpfFuncionario,dataNascimentoFuncionario;
-					codFuncionario = result.getString("cod_funcionario");
-					emailFuncionario = result.getString("email_funcionario");
 					nomeFuncionario = result.getString("nome_funcionario");
-					cpfFuncionario = result.getString("cpf_funcionario");
-					dataNascimentoFuncionario = result.getString("dt_nascimento_funcionario");
-					System.out.println(codFuncionario + "|" + emailFuncionario + "|" + nomeFuncionario + "|"
-					+ cpfFuncionario + "|" + dataNascimentoFuncionario);
+					cargoFuncionario = result.getString("cargo_funcionario");
+					matriculaFuncionario = result.getString("matricula_funcionario");
+					salarioFuncionario = result.getString("salario_funcionario");
+					telefoneFuncionario = result.getString("telefone_funcionario");
 				}
-				conexao.Desconectar();
+				System.out.println("Nome: "+ nomeFuncionario + "\n" + "Cargo: " + cargoFuncionario + "\n" +"Matricula: "
+						+ matriculaFuncionario + "\n" + "Salário: " + salarioFuncionario + "\n" + "Telefone: " + telefoneFuncionario);
 			} catch (SQLException e) {
 				e.printStackTrace();
 				e.getMessage();
+			}finally {
+				conexao.Desconectar();
 			}
 }
-	public void insertProduto() {
-		
-	}
-	
-	public void deleteProduto() {
-		
-		
-	}
-	
-	public void updateProduto() {
-		
-	}
-	
-	public void insertCliente() {
-		
-	}
-	
-	public void deleteCliente() {
-		
-	}
-	
-	public void updateCliente() {
-		
-	}
 	@Override
-	public void realizarAutenticacao(String email, String senha) {
+	public void realizarAutenticacao(String nome, String senha) {
 		try {
-			String emailFuncionario = null,senhaFuncionario = null;
+			String nomeFuncionario = null,senhaFuncionario = null;
 			conexao.Conectar();
-			String sql = "SELECT email_funcionario,senha_funcionario from funcionarios where cpf_funcionario = ?";
+			String sql = "SELECT nome_funcionario,senha_funcionario from funcionarios where matricula_funcionario = ?";
 			PreparedStatement preparedStatement = conexao.preparedStament(sql);
-			preparedStatement.setString(1, this.getCpf());
+			preparedStatement.setString(1, this.getMatricula());
 			ResultSet result = conexao.runPreparedSelect(preparedStatement);
 			while(result.next()) {
-				System.out.println(emailFuncionario = result.getString("email_funcionario"));
-				System.out.println(senhaFuncionario = result.getString("senha_funcionario"));
+				nomeFuncionario = result.getString("nome_funcionario");
+				senhaFuncionario = result.getString("senha_funcionario");
 			}
-			
-			if(email != emailFuncionario && senha != senhaFuncionario ) {
-				System.err.println("email ou senha incorretos");
-			}else{
+			System.out.println(nomeFuncionario + " | " + senhaFuncionario);			
+			if(nome.equals(nomeFuncionario) && senha.equals(senhaFuncionario)) {
 				System.out.println("Login realizado com sucesso!!!");
+			}else{
+				System.err.println("E-mail ou senha incorreto");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -103,8 +71,26 @@ public class Funcionario extends Pessoa implements Autenticacao{
 		}
 	}
 	
-	
-	
+//-------------- GETTERS E SETTERS ------------------//
+
+	public Cargos getCargo() {
+		return cargo;
+	}
+	public void setCargo(Cargos cargo) {
+		this.cargo = cargo;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 	
 	
 }
